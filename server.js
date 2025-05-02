b3c1a41240fc2e786fb0c54e7b9a7a113885acac
castleCodr <118486854+castleCodr@users.noreply.github.com>
2025-05-02 16:15:51 -0500

    Update server.js

--- server.js
+++ server.js
@@ -43,24 +43,19 @@ app.get('/auth/fitbit',(req,res,next) => {
   
   const { email,redirect } = req.query;
   console.log(email,redirect)
-
-  if (email && redirect) {
-    // Save email to session so it's available in callback
-    req.session.email = email;
-    req.session.redirect = redirect;
-
-  }
+  req.session.email = email || "";
+  req.session.redirect = redirect || "";
   passport.use(new FitbitStrategy({
     clientID: process.env.clientId,
     clientSecret: process.env.clientSecret,
     callbackURL: 'https://backendrepo-7lce.onrender.com/auth/fitbit/callback',
     scope: ['activity', 'heartrate', 'sleep', 'profile']
   },
-  async (accessToken, refreshToken, profile,req, done) => {
+  async (accessToken, refreshToken, profile, done) => {
     console.log('Fitbit Profile:', profile);
     console.log('Access Token:', accessToken);
     console.log('Refresh Token:', refreshToken);
-    await supabase.from('tokens').upsert({email: req.session.email,accessToken: accessToken,refreshToken: refreshToken})
+    await supabase.from('tokens').upsert({email: email,accessToken: accessToken,refreshToken: refreshToken})
 
     // Normally you'd save this info to your DB here
     return done(null, { profile, accessToken, refreshToken });
